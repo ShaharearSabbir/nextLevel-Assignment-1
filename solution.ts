2bbc81fb5020e9291d0fd55c5c6df29bb6bb8214
ShaharearSabbir <imshaharear@gmail.com>
2025-11-21 23:23:36 +0600

    solution 2

--- solution.ts
+++ solution.ts
@@ -1,14 +1,24 @@
-const formatValue = (value: string | number | boolean) => {
-  if (typeof value === "string") {
-    return value.toUpperCase();
-  } else if (typeof value === "number") {
-    return value * 10;
-  } else if (typeof value === "boolean") {
-    return !value;
+const formatValue = (
+  input: string | number | boolean
+): string | number | boolean | undefined => {
+  if (typeof input === "string") {
+    return input.toUpperCase();
+  } else if (typeof input === "number") {
+    return input * 10;
+  } else if (typeof input === "boolean") {
+    return !input;
   } else {
     return undefined;
   }
 };
 
-
+const getLength = (input: string | unknown[]): number | undefined => {
+  if (typeof input === "string") {
+    return input.length;
+  } else if (input instanceof Array) {
+    return input.length;
+  } else {
+    return undefined;
+  }
+};
 
